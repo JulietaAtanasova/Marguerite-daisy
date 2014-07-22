e91ceeb6cd2acc72b81c17eaa6f94e3cf482aae6
Vladislav Hadzhiyski <vladislav_hadzhiyski@hotmail.com>
2014-07-22 17:46:34 +0300

    game improvement

--- TeamWorkJavaScript/public_html/Script/game.js
+++ TeamWorkJavaScript/public_html/Script/game.js
@@ -2,53 +2,46 @@
 var player;
 
 function handler(event) {
-    var currentPos = {
-        x: player.x,
-        y: player.y
-    };
     if (event.keyCode == 39) { //right
-        player.move(10, 0);
-        canvas.width = canvas.width;
-        ctx.drawImage(player.img, currentPos.x, currentPos.y);
+        player.move(40, 0);
     }
     else if (event.keyCode == 37) { //left
-        player.move(-10, 0);
-        canvas.width = canvas.width;
-        ctx.drawImage(player.img, currentPos.x, currentPos.y);
+        player.move(-40, 0);
     }
     else if (event.keyCode == 40) { // down
-        player.move(0, 10);
-        canvas.width = canvas.width;
-        ctx.drawImage(player.img, currentPos.x, currentPos.y);
+        player.move(0, 60);
     }
     else if (event.keyCode == 38) { // up
-        player.move(0, -10);
-        canvas.width = canvas.width;
-        ctx.drawImage(player.img, currentPos.x, currentPos.y);
+        player.move(0, -60);
     }
+    canvas.width = canvas.width;
+    ctx.drawImage(player.img, player.x, player.y);
 }
 
 function Player(img, x, y) {
     var self = this;
     self.img = new Image();
+    self.img.onload = function () {
+        ctx.drawImage(self.img,40,60);
+    };
     self.img.src = img.src;
     self.x = x;
     self.y = y;
     self.move = function (x, y) {
-        if(self.x < 10){
-            self.x = 10;
+        if(self.x <= 0){
+            self.x = 1;
             return;
         }
-        else if(self.x > canvas.width - 65){
-            self.x = canvas.width - 65;
+        else if(self.x > canvas.width - self.img.width){
+            self.x = canvas.width - self.img.width;
             return;
         }
-        else if(self.y < 10){
-            self.y = 10;
+        else if(self.y < 0){
+            self.y = 1;
             return;
         }
-        else if(self.y > canvas.height - 100){
-            self.y = canvas.height - 100;
+        else if(self.y > canvas.height - self.img.height){
+            self.y = canvas.height - self.img.height;
             return;
         }
         else {
@@ -61,7 +54,7 @@ function Player(img, x, y) {
 function startGame() {
     canvas = document.getElementById('game');
     ctx = canvas.getContext('2d');
-    player = new Player({ src: "./IMG/hero.gif" }, 350, 250);
+    player = new Player({ src: "./IMG/hero.png" }, 40, 60);
     ctx.drawImage(player.img, player.x, player.y);
     document.addEventListener('keydown', handler, false);
 }
