d7c1596989bce806152ea8c6accaaf9800be551a
Vladislav Hadzhiyski <vladislav_hadzhiyski@hotmail.com>
2014-07-22 21:47:31 +0300

    player movement fix
    level movement completed !

--- TeamWorkJavaScript/public_html/Script/game.js
+++ TeamWorkJavaScript/public_html/Script/game.js
@@ -17,22 +17,22 @@ var puzzle = [
 
 function handler(event) {
     if (event.keyCode == 39) { //right 1
-        if(player.checkNextPosition([1, 0])){
+        if(player.checkNextPosition("right")){
             player.move(40, 0, 1);
         }
     }
     else if (event.keyCode == 37) { //left 2
-        if (player.checkNextPosition([-1, 0])) {
+        if (player.checkNextPosition("left")) {
             player.move(-40, 0, 2);
         }
     }
     else if (event.keyCode == 40) { // down 3
-        if (player.checkNextPosition([0, 1])) {
+        if (player.checkNextPosition("down")) {
             player.move(0, 60, 3);
         }
     }
     else if (event.keyCode == 38) { // up 4
-        if (player.checkNextPosition([0, -1])) {
+        if (player.checkNextPosition("up")) {
             player.move(0, -60, 4);
         }
     }
@@ -74,7 +74,7 @@ function Player(img, x, y) {
         else {
             player.x += x;
             player.y += y;
-            if (direction == 1) {
+            if (direction == 1) {1
                 player.matrix.col += 1;
             }
             else if (direction == 2) {
@@ -89,7 +89,20 @@ function Player(img, x, y) {
         }
     }
     self.checkNextPosition = function(direction){
-        if(puzzle[level - 1][player.matrix.row][player.matrix.col] != 0) {
+        var condition;
+        if(direction == "right"){
+            condition = (player.matrix.col + 1 < puzzle[level - 1][player.matrix.row].length) && (puzzle[level - 1][player.matrix.row][player.matrix.col + 1] != 0);
+        }
+        else if (direction == "left") {
+            condition = (player.matrix.col - 1 > 0) && (puzzle[level - 1][player.matrix.row][player.matrix.col - 1] != 0);
+        }
+        else if (direction == "up") {
+            condition = (player.matrix.row - 1 > 0) && (puzzle[level - 1][player.matrix.row - 1][player.matrix.col] != 0);
+        }
+        else if (direction == "down") {
+            condition = (player.matrix.row + 1 < puzzle[level - 1].length) && (puzzle[level - 1][player.matrix.row + 1][player.matrix.col] != 0);
+        }
+        if(condition){
             return true;
         }
         return false;
