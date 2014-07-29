a6997d05bae787f011fcc0300ed3ed885206b84a
Vladislav Hadzhiyski <vladislav_hadzhiyski@hotmail.com>
2014-07-29 14:06:16 +0300

    timer fix

--- TeamWorkJavaScript/public_html/Script/messages.js
+++ TeamWorkJavaScript/public_html/Script/messages.js
@@ -3,7 +3,7 @@ var messages = ['PAUSE','LEVEL COMPLETED','GAME OVER'];
 function getMessage (index){
     $(function () {
         $.get('message.html', function (data) {
-            $('#msg').append(data);
+            $('#msg').html(data);
         });
     });
 };
--- TeamWorkJavaScript/public_html/Script/timer.js
+++ TeamWorkJavaScript/public_html/Script/timer.js
@@ -1,5 +1,6 @@
 function Timer(seconds) {
   var self = this;
+  self.paused = false;
   self.time = seconds;
   self.minutes = seconds / 60 | 0;
   self.seconds = 0;
@@ -7,20 +8,37 @@ function Timer(seconds) {
   var a;
   self.start = function () {
     a = setInterval(function () {
-      self.time -= 1;
-      self.minutes = parseInt(self.time / 60, 10);
-      self.seconds = self.time % 60;
-      self.render();
+      if(self.paused === true){
+        return;
+      }
+      if(self.time > 0){
+        self.time -= 1;
+        self.minutes = parseInt(self.time / 60, 10);
+        self.seconds = self.time % 60;
+        self.render();
+      }
     }, self.INTERVAL);
-  };
+  }
   self.stop = function () {
     clearInterval(a);
-  };
+  }
   self.reset = function () {
     self.stop();
     self.time = seconds;
-  };
+  }
+  self.pause = function () {
+    if(self.paused === true){
+      self.paused = false;
+      return;
+    }
+    if(self.paused === false){
+      self.paused = true;
+      return;
+    }
+  }
   self.render = function () {
-    console.log(self.minutes + " : " + self.seconds);
+    var timeRender = self.time < 10 ? '0' + self.time : self.time; // only seconds
+    $('#countdown').text(timeRender);
+    //console.log(self.minutes + " : " + (self.seconds < 10 ? '0' + self.seconds : self.seconds));
   }
 }
